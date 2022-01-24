d624b71f46681b27cab6befc1244b161f1c770ff
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-01-24 21:25:31 +0900

    Create Min함수.java

---
+++ Level1/java/Min함수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		if(a>b) System.out.println(b);
+		else System.out.println(a);
+		
+	}
+}
