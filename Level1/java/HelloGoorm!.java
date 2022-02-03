284b07f64a31ebf7df50a5ef08ba7d3ec10d07f6
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-03 12:32:16 +0900

    Create HelloGoorm!.java

---
+++ Level1/java/HelloGoorm!.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine());
+		
+		for(int i=1; i<=input; i++)
+			System.out.println("Hello Goorm !");
+	}
+}
