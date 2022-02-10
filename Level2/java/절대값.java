19c11461c9b044bce65578f3c85146215be50111
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-10 13:17:20 +0900

    Create 절대값.java

---
+++ Level2/java/절대값.java
@@ -0,0 +1,9 @@
+import java.io.*;
+class Main {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int input = Integer.parseInt(br.readLine());
+		
+		System.out.println(Math.abs(input));
+	}
+}
