be8b4d73c2fbfff86a2794fb72b58f6343d7c6fa
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-02-15 17:11:49 +0900

    Create 좀비런.java

---
+++ Level2/java/좀비런.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int cnt = sc.nextInt();
+		int arr[]=new int[cnt];
+		
+		for(int i=0; i<cnt; i++)
+			arr[i]=sc.nextInt();
+		
+		Arrays.sort(arr);
+		
+		System.out.println(arr[cnt-1]);
+	}
+}
